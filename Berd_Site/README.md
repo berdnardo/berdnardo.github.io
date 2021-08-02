7e2a96205aa4efce31fb0342ac946d5e5322ce04
berdnardo <berdnardo@gmail.com>
2021-08-01 21:21:32 -0300

    configiração inicial

---
+++ Berd_Site/README.md
@@ -0,0 +1,1 @@
+# Site Pessoal com React no Github Pages
---
+++ Berd_Site/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Profile from './layout/profile';
+import "@fontsource/roboto";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Profile />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
