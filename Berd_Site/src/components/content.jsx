c96fb3ea25cccbc4e37c7797e37e36853b992b2c
berdnardo <berdnardo@gmail.com>
2021-08-03 22:25:19 -0300

    pré css profile

---
+++ Berd_Site/src/components/content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Content() {
+
+  return (
+    <>
+    Content
+    </>
+  )
+
+}
+
+export default Content
--- Berd_Site/src/layout/profile.jsx
+++ Berd_Site/src/layout/profile.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Content from '../components/content'
+import '../assets/css/Profile.css'
+
+
 
 function Profile() {
 
-
   return (
-    <>
-    Bernardo
-    </>
+    <div className='Principal'>
+      <div className='Header'><Header/></div>
+      <div className='Content'><Content/></div>
+      <div className='Footer'><Footer/></div>      
+    </div>
+    
   )
 
 }
